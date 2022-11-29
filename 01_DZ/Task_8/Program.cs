707c9c675ce9768845721192defb3ccacd5f0afc
be_red <bered321@inbox.ru>
2022-11-29 13:27:03 +0300

    add solution task 8

--- 01_DZ/Task_8/Program.cs
+++ 01_DZ/Task_8/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i < a; i++)
+{
+    if (i % 2 == 0)
+        Console.Write($"{i} ");
+}
+
