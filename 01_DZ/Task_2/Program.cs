ee4a0c22934fedd8748fd82d1509f1b693b8e5e7
be_red <bered321@inbox.ru>
2022-11-29 15:06:12 +0300

    add min

--- 01_DZ/Task_2/Program.cs
+++ 01_DZ/Task_2/Program.cs
@@ -4,8 +4,8 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
 if (a < b)
-    Console.WriteLine($"max = {b}");
+    Console.WriteLine($"max = {b}, min = {a}");
 else if (a == b)
     Console.WriteLine("Числа равны");
 else
-    Console.WriteLine($"max = {a}");
+    Console.WriteLine($"max = {a}, min = {b}");
