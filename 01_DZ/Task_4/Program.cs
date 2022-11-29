78d43e2faaf93683e3325c7785ada5251ccfedb8
be_red <bered321@inbox.ru>
2022-11-29 13:04:22 +0300

    add solution task 4

--- 01_DZ/Task_4/Program.cs
+++ 01_DZ/Task_4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c)
+    Console.WriteLine($"max = {a}");
+else if (b > a && b > c)
+    Console.WriteLine($"max = {b}");
+else if (a == b && b == c)
+    Console.WriteLine("Числа равны");
+else
+    Console.WriteLine($"max = {c}");
