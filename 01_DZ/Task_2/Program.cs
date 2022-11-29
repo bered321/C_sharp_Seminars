b5689509119ad035a5efbf916e27a4450bd5ec6a
be_red <bered321@inbox.ru>
2022-11-29 12:46:38 +0300

    delete {

--- 01_DZ/Task_2/Program.cs
+++ 01_DZ/Task_2/Program.cs
@@ -4,9 +4,7 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
 if (a < b)
-{
     Console.WriteLine($"max = {b}");
-}
 else if (a == b)
     Console.WriteLine("Числа равны");
 else
