7effea053f649a5c281545d080384d1c98da698f
be_red <bered321@inbox.ru>
2022-12-02 14:00:33 +0300

    add solution task 10

--- 01_DZ/Task_10/Program.cs
+++ 01_DZ/Task_10/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите любое трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 100 && a < 1000)
+{
+    int n1 = a % 100;
+    int n2 = n1 / 10;
+    Console.WriteLine($"Вторая цифра этого числа: {n2}");
+}
+else
+    Console.WriteLine("Введено не верное число!");
+
