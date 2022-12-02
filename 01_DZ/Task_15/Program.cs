0b851ae59836fa5e3f1aec2cf65eeec5e2567712
be_red <bered321@inbox.ru>
2022-12-02 14:44:46 +0300

    add solution task 15

--- 01_DZ/Task_15/Program.cs
+++ 01_DZ/Task_15/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 7 || a < 1)
+{
+    Console.WriteLine("Введен не верный номер дня недели");
+}
+else if (a == 6 || a == 7)
+    Console.WriteLine("Выходной");
+else if (a > 0 || a < 7)
+    Console.WriteLine("Будний");
