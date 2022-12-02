a66648e102f0255a16a23b3c2514dffedb83433c
be_red <bered321@inbox.ru>
2022-12-02 17:08:06 +0300

    add solution task dop

--- 01_DZ/Task_Dop/Program.cs
+++ 01_DZ/Task_Dop/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введи последовательность чисел: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int max1 = a;
+int max2 = a;
+while (a != 0)
+{
+    a = Convert.ToInt32(Console.ReadLine());
+    if (a > max1)
+    {
+        max2 = max1;
+        max1 = a;
+    }
+    else if (a > max2 && a < max1)
+        {
+            max2 = a;
+        }
+    
+}
+Console.WriteLine();
+Console.WriteLine($"Второй максимум {max2}");
