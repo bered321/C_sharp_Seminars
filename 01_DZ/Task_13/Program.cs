b94afd13f463114bf925ce466f119643577b0d19
be_red <bered321@inbox.ru>
2022-12-02 14:32:26 +0300

    add solution task 13

--- 01_DZ/Task_13/Program.cs
+++ 01_DZ/Task_13/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите любое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; a > 1000; i++)
+{
+    a = a / 10;       
+}
+if (a > 100 && a < 1000)
+{
+    int n1 = a % 10;
+    Console.WriteLine($"Третья цифра этого числа: {n1}");
+}
+else if (a < 100)
+    Console.WriteLine("Третьего числа нет");
+
+
