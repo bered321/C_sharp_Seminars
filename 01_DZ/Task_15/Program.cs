7a9710034d91fc46a86f83388e296df64ff7c552
be_red <bered321@inbox.ru>
2022-12-02 14:45:21 +0300

    add solution task 15

--- 01_DZ/Task_15/Program.cs
+++ 01_DZ/Task_15/Program.cs
@@ -2,9 +2,7 @@
 Console.Write("Введите номер дня недели: ");
 int a = Convert.ToInt32(Console.ReadLine());
 if (a > 7 || a < 1)
-{
     Console.WriteLine("Введен не верный номер дня недели");
-}
 else if (a == 6 || a == 7)
     Console.WriteLine("Выходной");
 else if (a > 0 || a < 7)
