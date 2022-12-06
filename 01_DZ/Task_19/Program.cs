6a4839ff55c87ebf55e5e5c2222aaacc96915895
be_red <bered321@inbox.ru>
2022-12-06 16:00:28 +0300

    add solution Task 19

--- 01_DZ/Task_19/Program.cs
+++ 01_DZ/Task_19/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int forward = Convert.ToInt32(Console.ReadLine());
+int palindrom = 0;
+while (forward < 10000 || forward >= 100000)
+{
+    Console.WriteLine("Вы ошиблись! \nВведите пятизначное число: ");
+    forward = Convert.ToInt32(Console.ReadLine());
+}
+int N = forward.ToString().Length - 1;
+string b = forward.ToString();
+for (int i = 0; i < N / 2; i++)
+{
+    if (b[i] == b[N - i])
+        palindrom = 1;
+}
+if (palindrom == 1)
+    Console.WriteLine("Число является палиндромом");
+else
+    Console.WriteLine("Число является не палиндромом");
