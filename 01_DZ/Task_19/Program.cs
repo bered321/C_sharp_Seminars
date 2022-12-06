2656de7ba79421cd4c7147c8b8795e6c726f56f5
be_red <bered321@inbox.ru>
2022-12-06 16:30:26 +0300

    error correction

--- 01_DZ/Task_19/Program.cs
+++ 01_DZ/Task_19/Program.cs
@@ -12,7 +12,9 @@ int N = forward.ToString().Length - 1;
 string b = forward.ToString();
 for (int i = 0; i < N / 2; i++)
 {
-    if (b[i] == b[N - i])
+    if (b[i] != b[N - i])
+        palindrom = 0;
+    else
         palindrom = 1;
 }
 if (palindrom == 1)
