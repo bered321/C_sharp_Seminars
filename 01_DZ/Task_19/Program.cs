ab34514946a18b0fd52c349231717c9c5a47fbe4
be_red <bered321@inbox.ru>
2022-12-06 16:17:17 +0300

    add mod solution Task 19

--- 01_DZ/Task_19/Program.cs
+++ 01_DZ/Task_19/Program.cs
@@ -2,20 +2,17 @@
 Console.Clear();
 Console.Write("Введите пятизначное число: ");
 int forward = Convert.ToInt32(Console.ReadLine());
-int palindrom = 0;
 while (forward < 10000 || forward >= 100000)
 {
     Console.WriteLine("Вы ошиблись! \nВведите пятизначное число: ");
     forward = Convert.ToInt32(Console.ReadLine());
 }
-int N = forward.ToString().Length - 1;
 string b = forward.ToString();
-for (int i = 0; i < N / 2; i++)
+for (int i = 0; i < (forward.ToString().Length - 1) / 2; i++)
 {
-    if (b[i] == b[N - i])
-        palindrom = 1;
+    if (b[i] == b[forward.ToString().Length - 1 - i])
+        Console.WriteLine("Число является палиндромом");
+    else
+        Console.WriteLine("Число является не палиндромом");
+        break;
 }
-if (palindrom == 1)
-    Console.WriteLine("Число является палиндромом");
-else
-    Console.WriteLine("Число является не палиндромом");
