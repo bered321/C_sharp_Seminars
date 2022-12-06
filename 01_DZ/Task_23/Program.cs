0658e119b61c8a9507713148a70f3d7a195ec634
be_red <bered321@inbox.ru>
2022-12-06 11:23:43 +0300

    add solution Task 23

--- 01_DZ/Task_23/Program.cs
+++ 01_DZ/Task_23/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= a; i++)
+    Console.Write($"{Math.Pow(i, 3)} ");
