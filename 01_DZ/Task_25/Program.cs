c21ced31f1367d60f9f61837faadc81a5604f06f
be_red <bered321@inbox.ru>
2022-12-11 19:08:36 +0300

    add solution Task 25

--- 01_DZ/Task_25/Program.cs
+++ 01_DZ/Task_25/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a;
+for (int i = 1; i < b; i++)
+{
+   result = result*a;
+    
+}
+Console.WriteLine($"Число {a} в степени {b} равно: {result}");
