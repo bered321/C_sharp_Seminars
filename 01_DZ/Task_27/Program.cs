2e5c775c2a49e65762baf654e8151fc3f18e2309
be_red <bered321@inbox.ru>
2022-12-11 20:09:37 +0300

    add solution Task 27

--- 01_DZ/Task_27/Program.cs
+++ 01_DZ/Task_27/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число : ");
+string? a = Console.ReadLine();
+int[] b = new int[a.Length];
+int sum = 0;
+for (int i = 0; i < a.Length; i++)
+{
+    b[i]=Convert.ToInt32(a[i].ToString());
+    sum = sum + b[i];
+}
+Console.WriteLine($"Сумма цифр в числе равна: {sum}");
