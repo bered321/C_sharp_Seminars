48ff75b967261cb0504b07abe694af1daa22d355
be_red <bered321@inbox.ru>
2022-12-12 18:32:20 +0300

    add solution Task Dop4

--- 01_DZ/Task_Dop4/Program.cs
+++ 01_DZ/Task_Dop4/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool Prostoe(int number)
+{
+    if (number < 2) return false;
+    if (number == 2) return true;
+    if (number % 2 == 0) return false;
+    for (int i = 3; (i * i) <= number; i += 2)
+    {
+        if (number % i == 0) return false;
+    }
+    return true;
+}
+
+
+Console.Clear();
+Console.Write("Введите четное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n <= 3 || n >= 999 || n % 2 != 0)
+{
+    Console.WriteLine("Вы ошиблись!\nВведите правильное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 2; i < n; i++)
+{
+    Prostoe(i);
+    Prostoe(n - i);
+    if (Prostoe(i) == true && Prostoe(n - i) == true)
+    {
+        Console.WriteLine($"Первое простое слогаемое: {i} \nВторое простое слогаемое: {n - i}");
+        break;
+    }
+}
+
+
+
