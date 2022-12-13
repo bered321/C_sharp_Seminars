6b62d737340e6ff5e7f0d596cbfdd3cd488854ff
be_red <bered321@inbox.ru>
2022-12-13 18:44:52 +0300

    add solution Task 34

--- 01_DZ/Task_34/Program.cs
+++ 01_DZ/Task_34/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000); // [1, 10]
+}
+
+int count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 0)
+            count = count + 1;
+    return count;
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Количество четных чисел в массиве: {count(array)}");
