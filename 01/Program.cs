631eeed2755205dc79a2f40be4c6d96d7b854379
be_red <bered321@inbox.ru>
2022-12-13 18:22:02 +0300

    add new folder Task 34

--- 01/Program.cs
+++ 01/Program.cs
@@ -282,3 +282,105 @@ for (int i = 1; i <= a; i++)
 // }
 // Console.WriteLine($"[{string.Join(", ", array)}]");
 
+// //Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+//
+// void InputArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(-9, 10); // [-9, 9]
+// }
+// void ReleaseArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] *= (-1);
+// }
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[n];
+// InputArray(array);
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// ReleaseArray(array);
+// Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
+
+
+
+// // Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// // 4; массив [6, 7, 19, 345, 3] -> нет
+// // -3; массив [6, 7, 19, 345, 3] -> да
+// //
+// void InputArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(-9, 10); // [-9, 9]
+// }
+// string ReleaseArray(int[] array, int k)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] == k)
+//             return "yes";
+//     }
+//     return "no";
+// }
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[n];
+// InputArray(array);
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// Console.Write("Введите число: ");
+// int k = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(ReleaseArray(array, k));
+
+// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+// void InputArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(-100, 101); // [-100, 100]
+// }
+// int ReleaseArray(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] >= 10 && array[i] <= 99)
+//             count++; // count = count + 1;
+//     }
+//     return count;
+// }
+// Console.Clear();
+// int[] array = new int[123];
+// InputArray(array);
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// Console.WriteLine(ReleaseArray(array));
+
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+//
+// void InputArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(1, 11); // [1, 10]
+// }
+// int[] ReleaseArray(int[] array)
+// {
+//     int[] result = new int[array.Length / 2 + array.Length % 2];
+//     for (int i = 0; i < array.Length / 2 + array.Length % 2; i++)
+//         result[i] = array[i] * array[array.Length - 1 - i];
+//     return result;
+// }
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[n];
+// InputArray(array);
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// Console.WriteLine($"Конечный массив: [{string.Join(", ", ReleaseArray(array))}]");
