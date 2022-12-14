4a1f372a8b1110aca3e4a20fa8115007ff4f350c
be_red <bered321@inbox.ru>
2022-12-14 17:22:13 +0300

    add solution Task Dop5

--- 01_DZ/Task_Dop5/Program.cs
+++ 01_DZ/Task_Dop5/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 32);
+}
+
+void day(int[] array)
+{
+    int chet = 0;
+    int nechet = 0;
+    Console.Write("Дни когда Вася получил 4: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            chet += 1;
+            Console.Write($"{array[i]} ");
+        }
+    }
+    Console.WriteLine();
+    Console.Write("Дни когда Вася получил 3: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0)
+        {
+            nechet += 1;
+            Console.Write($"{array[i]} ");
+        }
+    }
+    Console.WriteLine();
+    if (chet >= nechet)
+        Console.WriteLine("Вася может рассчитывать на оценку 4");
+    else
+        Console.WriteLine("Вася может рассчитывать на оценку 3");
+}
+
+
+Console.Clear();
+int[] array = new int[10];
+InputArray(array);
+Console.WriteLine($"Дни, когда были уроки английского: {string.Join(", ", array)}");
+day(array);
