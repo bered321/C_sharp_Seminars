5d42090e1056e25470550c1e96336ebf357fa306
be_red <bered321@inbox.ru>
2022-12-14 13:33:16 +0300

    add solution Task 36

--- 01_DZ/Task_36/Program.cs
+++ 01_DZ/Task_36/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-20, 21); 
+}
+
+int sum(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i+=2)
+        sum=sum+array[i];
+    return sum;
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Сумма элементов с нечетными индексами в массиве равна: {sum(array)}");
