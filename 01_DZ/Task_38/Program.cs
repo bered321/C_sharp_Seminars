1457bc9039db58048f7e8b23d3964bc24317b567
be_red <bered321@inbox.ru>
2022-12-14 15:17:51 +0300

    add solution Task 38

--- 01_DZ/Task_38/Program.cs
+++ 01_DZ/Task_38/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Math.Round(new Random().NextDouble() * (20 - 10) + 10, 2);
+}
+
+double minmax(double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+            max = array[i];
+        if (min > array[i])
+            min = array[i];
+    }
+    return (Math.Round(max - min, 2));
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+InputArray(array);
+Console.WriteLine($"Массив: [{string.Join("; ", array)}]");
+Console.WriteLine($"Разница между max и min элементами массива: {minmax(array)}");
+
+
