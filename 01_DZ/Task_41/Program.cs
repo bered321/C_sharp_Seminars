60fca1328d61a7985b51f7f58d7f124dda960d7f
be_red <bered321@inbox.ru>
2022-12-16 12:45:47 +0300

    add solution Task 41

--- 01_DZ/Task_41/Program.cs
+++ 01_DZ/Task_41/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите числа через пробел: ");
+int count = 0;
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray(); //в одну строчку через пробел
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+        count += 1;
+}
+Console.WriteLine($"Пользователь ввел {count} положительных чисел");
