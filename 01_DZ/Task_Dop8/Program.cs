695468d50bc963b73be0b6a9125bdbfc41d2e1ce
be_red <bered321@inbox.ru>
2022-12-26 21:38:35 +0300

    add solution Task Dop8

--- 01_DZ/Task_Dop8/Program.cs
+++ 01_DZ/Task_Dop8/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Matrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 51); 
+    }
+}
+
+void Result(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0) / 2; i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[matrix.GetLength(0) - 1 - i, j];
+            matrix[matrix.GetLength(0) - 1 - i, j] = temp;
+        }
+    }
+}
+
+void Print(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+Matrix(matrix);
+Console.WriteLine("Начальный массив:");
+Print(matrix);
+Result(matrix);
+Console.WriteLine("Конечный массив:");
+Print(matrix);
