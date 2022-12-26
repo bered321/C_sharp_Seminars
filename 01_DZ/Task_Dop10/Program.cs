66d8d09b3adb682861e1cfeeb02d4ef563c61985
be_red <bered321@inbox.ru>
2022-12-26 22:14:15 +0300

    add new folder Task Dop9

--- 01_DZ/Task_Dop10/Program.cs
+++ 01_DZ/Task_Dop10/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+int number = 0;
+for (int i = 1; i < matrix.GetLength(0) + matrix.GetLength(1) + 1; i++)
+{
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            if (j + k + 1 == i)
+            {
+                matrix[j, k] = number;
+                number++;
+            }
+        }
+    }
+}
+PrintMatrix(matrix);
