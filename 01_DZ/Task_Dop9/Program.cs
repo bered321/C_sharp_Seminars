521888fdba93410a00a733fc38b3e866ece22fb3
be_red <bered321@inbox.ru>
2022-12-26 22:51:26 +0300

    add solution Task Dop9

--- 01_DZ/Task_Dop9/Program.cs
+++ 01_DZ/Task_Dop9/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FirstMatrix(int[,] MatrixOne)
+{
+    Console.WriteLine();
+    Console.WriteLine("Введите исходное бинарное черно-белое изображение:");
+    for (int i = 0; i < MatrixOne.GetLength(0); i++)
+    {
+        string? input = Console.ReadLine();
+        for (int j = 0; j < input.Length; j++)
+        {
+            MatrixOne[i, j] = input[j];
+        }
+    }
+}
+ 
+void SecondMatrix(int[,] MatrixTwo)
+{
+    Console.WriteLine("Введите полученный Мишиной программой негатив:");
+    for (int i = 0; i < MatrixTwo.GetLength(0); i++)
+    {
+        string? input = Console.ReadLine();
+        for (int j = 0; j < input.Length; j++)
+        {
+            MatrixTwo[i, j] = input[j];
+        }
+    }
+}
+ 
+void Result(int[,] MatrixOne, int[,] MatrixTwo)
+{
+    int count = 0;
+    for (int i = 0; i < MatrixTwo.GetLength(0); i++)
+    {
+        for (int j = 0; j < MatrixTwo.GetLength(1); j++)
+        {
+            if (MatrixOne[i, j] == MatrixTwo[i, j])
+                count++;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Число неправильно сформированых пикселей негатива равно: {count}");
+}
+ 
+Console.Clear();
+Console.Write("Введите через пробел кол-во строк и столбцов: ");
+string? numbers = Console.ReadLine(); 
+int n = Convert.ToInt32(numbers[0].ToString());
+int m = Convert.ToInt32(numbers[2].ToString());
+int[,] MatrixOne = new int[n, m];
+int[,] MatrixTwo = new int[n, m];
+FirstMatrix(MatrixOne);
+Console.WriteLine();
+SecondMatrix(MatrixTwo);
+Result(MatrixOne, MatrixTwo);
