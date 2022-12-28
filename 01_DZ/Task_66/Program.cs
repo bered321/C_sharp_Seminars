8a65161df6c0f1f69ac361d23bad183393c84128
be_red <bered321@inbox.ru>
2022-12-28 20:33:11 +0300

    add solution Task 66

--- 01_DZ/Task_66/Program.cs
+++ 01_DZ/Task_66/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int rec(int m, int n)
+{
+    if (m == n)
+        return n;
+    return m + rec(m + 1, n);
+}
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N равна: {rec(m, n)}");
