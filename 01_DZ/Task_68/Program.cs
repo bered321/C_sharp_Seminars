7d8068024e703df83d26e640f14573c3058a60ce
be_red <bered321@inbox.ru>
2022-12-28 20:46:45 +0300

    add solution Task 68

--- 01_DZ/Task_68/Program.cs
+++ 01_DZ/Task_68/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int rec(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return rec(m - 1, 1);
+    else 
+        return rec(m - 1, rec(m, n - 1));
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Результат вычисления функции Аккермана: {rec(m, n)}");
